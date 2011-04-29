b2d7d1459badb13676fb659cb64cc05f39e425c0
Felipe Lavín Z <felipe.lavin@gmail.com@f19442fc-cf77-11dd-a2b5-e5f827957e07>
2011-04-29 03:40:04 +0000

    * Created mo files for contributed translations: ** Catalán by Isaac Sanchez ** Hebrew by Ammar Al Rashed ** Italian by Alessandro Segala ** Norwegian Nynorks by (unknown) * Fixed translations loading issue * Micro optimizations for performance

--- header.php
+++ header.php
@@ -1,26 +1,27 @@
+<?php load_theme_textdomain( 'sandbox', TEMPLATEPATH .'/translation' ); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes() ?>>
 <head profile="http://gmpg.org/xfn/11">
 	<title><?php wp_title( '-', true, 'right' ); echo wp_specialchars( get_bloginfo('name'), 1 ) ?></title>
-	<meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />	
+	<meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url') ?>" />
-	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory') ?>/css/fancy.yuic.css" />			
+	<link rel="stylesheet" type="text/css" href="<?php echo TEMPLATE_URL ?>/css/fancy.yuic.css" />
 	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url') ?>" title="<?php printf( __( '%s latest posts', 'sandbox' ), wp_specialchars( get_bloginfo('name'), 1 ) ) ?>" />
 	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url') ?>" title="<?php printf( __( '%s latest comments', 'sandbox' ), wp_specialchars( get_bloginfo('name'), 1 ) ) ?>" />
-	<link rel="pingback" href="<?php bloginfo('pingback_url') ?>" />	
-	<script type="text/javascript" src="<?php bloginfo('template_directory')?>/js/jquery.min.js"></script>
-	<script type="text/javascript" src="<?php bloginfo('template_directory')?>/js/jquery.fancybox.yuic.js"></script>
-	<!--[if lt IE 7]><script src="<?php bloginfo('template_directory')?>/js/jquery.pngFix.pack.js" type="text/javascript"></script><![endif]-->
-	<script type="text/javascript" src="<?php bloginfo('template_directory')?>/js/satorii.js"></script>
-	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); /* Threaded comments */?>	
-	<?php wp_head() /* For plugins */?>	
+	<link rel="pingback" href="<?php bloginfo('pingback_url') ?>" />
+	<script type="text/javascript" src="<?php echo TEMPLATE_URL ?>/js/jquery.min.js"></script>
+	<script type="text/javascript" src="<?php echo TEMPLATE_URL ?>/js/jquery.fancybox.yuic.js"></script>
+	<!--[if lt IE 7]><script src="<?php echo TEMPLATE_URL ?>/js/jquery.pngFix.pack.js" type="text/javascript"></script><![endif]-->
+	<script type="text/javascript" src="<?php echo TEMPLATE_URL ?>/js/satorii.js"></script>
+	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); /* Threaded comments */?>
+	<?php wp_head() /* For plugins */?>
 </head>
 <body class="<?php sandbox_body_class() ?>">
 
 <div id="wrapper" class="hfeed">
 
 	<div id="header">
-		<h1 id="blog-title"><span><a href="<?php bloginfo('home') ?>/" title="<?php echo wp_specialchars( get_bloginfo('name'), 1 ) ?>" rel="home"><?php bloginfo('name') ?></a></span></h1>
+		<h1 id="blog-title"><span><a href="<?php bloginfo('home') ?>/" title="<?php echo esc_attr( get_bloginfo('name') ) ?>" rel="home"><?php bloginfo('name') ?></a></span></h1>
 		<div id="blog-description"><?php bloginfo('description') ?></div>
 	</div><!--  #header -->
 
