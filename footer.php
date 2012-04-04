5c87bb7cc08f89171d152383f1679c1cbb6b1a9c
Felipe Lavín Z <felipe@yukei.net>
2012-04-04 03:56:44 +0000

    css sticky footer

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
-	<div id="footer">
-		<p><strong><?php bloginfo('name')?></strong> <a href="<?php bloginfo('rss2_url') ?>">(RSS)</a> + <strong>Sator-ii</strong> theme by <a href="http://yukei.net">Felipe Lavín</a></p>
-		<?php wp_footer() ?>
-	</div><!-- #footer -->
+	</div><!-- #inner-wrap -->
 </div><!-- #wrapper .hfeed -->
+<div id="footer">
+	<p><strong><?php bloginfo('name')?></strong> <a href="<?php bloginfo('rss2_url') ?>">(RSS)</a> + <strong>Sator-ii</strong> theme by <a href="http://yukei.net">Felipe Lavín</a></p>
+	<?php wp_footer() ?>
+</div><!-- #footer -->
 </body>
 </html>
