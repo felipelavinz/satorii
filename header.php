d220523588d2955b2581459c2418910a94d4c728
felipe.lavin@gmail.com <felipe.lavin@gmail.com@f19442fc-cf77-11dd-a2b5-e5f827957e07>
2012-04-04 03:56:44 +0000

    css sticky footer

--- header.php
+++ header.php
@@ -11,7 +11,12 @@
 	<link rel="pingback" href="<?php bloginfo('pingback_url') ?>" />
 	<script type="text/javascript" src="<?php echo TEMPLATE_URL ?>/js/jquery.min.js"></script>
 	<script type="text/javascript" src="<?php echo TEMPLATE_URL ?>/js/jquery.fancybox.yuic.js"></script>
-	<!--[if lt IE 7]><script src="<?php echo TEMPLATE_URL ?>/js/jquery.pngFix.pack.js" type="text/javascript"></script><![endif]-->
+	<!--[if lt IE 7]>
+		<script src="<?php echo TEMPLATE_URL ?>/js/jquery.pngFix.pack.js" type="text/javascript"></script>
+	<![endif]-->
+	<!--[if (lt IE 7) | (IE 8)]>
+		<style type="text/css">#wrapper{ display: table; height: 100% }</style>
+	<![endif]-->
 	<script type="text/javascript" src="<?php echo TEMPLATE_URL ?>/js/satorii.js"></script>
 	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); /* Threaded comments */?>
 	<?php wp_head() /* For plugins */?>
@@ -19,13 +24,13 @@
 <body class="<?php sandbox_body_class() ?>">
 
 <div id="wrapper" class="hfeed">
+	<div id="inner-wrap">
+		<div id="header">
+			<h1 id="blog-title"><span><a href="<?php bloginfo('home') ?>/" title="<?php echo esc_attr( get_bloginfo('name') ) ?>" rel="home"><?php bloginfo('name') ?></a></span></h1>
+			<div id="blog-description"><?php bloginfo('description') ?></div>
+		</div><!--  #header -->
 
-	<div id="header">
-		<h1 id="blog-title"><span><a href="<?php bloginfo('home') ?>/" title="<?php echo esc_attr( get_bloginfo('name') ) ?>" rel="home"><?php bloginfo('name') ?></a></span></h1>
-		<div id="blog-description"><?php bloginfo('description') ?></div>
-	</div><!--  #header -->
-
-	<div id="access">
-		<div class="skip-link"><a href="#content" title="<?php _e( 'Skip to content', 'sandbox' ) ?>"><?php _e( 'Skip to content', 'sandbox' ) ?></a></div>
-		<?php satorii_globalnav(); ?>
-	</div><!-- #access -->
+		<div id="access">
+			<div class="skip-link"><a href="#content" title="<?php _e( 'Skip to content', 'sandbox' ) ?>"><?php _e( 'Skip to content', 'sandbox' ) ?></a></div>
+			<?php satorii_globalnav(); ?>
+		</div><!-- #access -->
