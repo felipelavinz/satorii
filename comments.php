dfca46d671bec15a9c7956240a5216414cbcabfc
Felipe Lavín Z <felipe@yukei.net>
2014-02-10 23:25:21 -0300

    use comment_form()

--- comments.php
+++ comments.php
@@ -65,8 +65,7 @@ foreach ( $comments as $comment )
 <?php if ( 'open' == $post->comment_status ) : ?>
 <?php $req = get_option('require_name_email'); // Checks if fields are required. Thanks, Adam. ;-) ?>
 
-				<div id="respond"<?php if ( $comment_count == 0 ) : echo ' class="no-replies"'; endif; ?>>
-					<h3><?php _e( 'Post a Comment', 'satorii' ) ?></h3>
+				<div id="respondcontainer"<?php if ( $comment_count == 0 ) : echo ' class="no-replies"'; endif; ?>>
 
 <?php if ( get_option('comment_registration') && !$user_ID ) : ?>
 					<p id="login-req"><?php printf(__('You must be <a href="%s" title="Log in">logged in</a> to post a comment.', 'satorii'),
@@ -74,44 +73,23 @@ foreach ( $comments as $comment )
 
 <?php else : ?>
 					<div class="formcontainer">
-						<form id="commentform" action="<?php bloginfo('wpurl') ?>/wp-comments-post.php" method="post">
-
-<?php if ( $user_ID ) : ?>
-							<p id="login"><?php printf( __( '<span class="loggedin">Logged in as <a href="%1$s" title="Logged in as %2$s">%2$s</a>.</span> <span class="logout"><a href="%3$s" title="Log out of this account">Log out?</a></span>', 'satorii' ),
-								get_bloginfo('wpurl') . '/wp-admin/profile.php',
-								esc_html( $user_identity, 1 ),
-								wp_logout_url(get_permalink()) ) ?></p>
-
-<?php else : ?>
-
-							<p id="comment-notes"><?php _e( 'Your email is <em>never</em> shared.', 'satorii' ) ?> <?php if ($req) _e( 'Required fields are marked <span class="required">*</span>', 'satorii' ) ?></p>
-
-							<div class="yui-gf">
-							<div class="yui-u first">
-							<div class="form-label"><label for="author"><?php _e( 'Name', 'satorii' ) ?></label> <?php if ($req) _e( '<span class="required">*</span>', 'satorii' ) ?></div>
-							<div class="form-input"><input id="author" name="author" class="text<?php if ($req) echo ' required'; ?>" type="text" value="<?php echo $comment_author ?>" size="30" maxlength="50" tabindex="3" /></div>
-
-							<div class="form-label"><label for="email"><?php _e( 'Email', 'satorii' ) ?></label> <?php if ($req) _e( '<span class="required">*</span>', 'satorii' ) ?></div>
-							<div class="form-input"><input id="email" name="email" class="text<?php if ($req) echo ' required'; ?>" type="text" value="<?php echo $comment_author_email ?>" size="30" maxlength="50" tabindex="4" /></div>
-
-							<div class="form-label"><label for="url"><?php _e( 'Website', 'satorii' ) ?></label></div>
-							<div class="form-input"><input id="url" name="url" class="text" type="text" value="<?php echo $comment_author_url ?>" size="30" maxlength="50" tabindex="5" /></div>
-							</div>
-
-<?php endif // REFERENCE: * if ( $user_ID ) ?>
-
-							<div class="yui-u" id="form-textarea">
-							<div class="cancel-comment-reply"><?php cancel_comment_reply_link() ?></div>
-							<div class="form-label"><label for="comment"><?php _e( 'Comment', 'satorii' ) ?></label></div>
-							<div class="form-textarea"><textarea id="comment" name="comment" class="text required" cols="45" rows="8" tabindex="6"></textarea></div>
-
-							<div class="form-submit"><input id="submit" name="submit" class="button" type="submit" value="<?php _e( 'Post Comment', 'satorii' ) ?>" tabindex="7" /><?php comment_id_fields(); ?></div>
-							</div>
-							</div>
-
-							<div class="form-option"><?php do_action( 'comment_form', $post->ID ) ?></div>
-
-						</form><!-- #commentform -->
+						<?php comment_form(array(
+							'fields' => array(
+								'author' => '<div class="yui-gf">
+									<div class="yui-u first">
+									<div class="form-label"><label for="author">'. __( 'Name', 'satorii' ) .'</label> '. ( $req ? __( '<span class="required">*</span>', 'satorii' ) : '' ) .'</div>
+									<div class="form-input"><input id="author" name="author" class="text'. ( $req ? ' required' : '' ) .'" type="text" value="'. esc_attr($comment_author) .'" size="30" maxlength="50" tabindex="3" /></div>',
+								'email' => '<div class="form-label"><label for="email">'. __( 'Email', 'satorii' ) .'</label> '. ( $req ? __( '<span class="required">*</span>', 'satorii' ) : '' ) .'</div>
+									<div class="form-input"><input id="email" name="email" class="text'. ( $req ? ' required' : '' ) .'" type="text" value="'. esc_attr($comment_author_email) .'" size="30" maxlength="50" tabindex="4" /></div>',
+								'url' => '<div class="form-label"><label for="url">'. __( 'Website', 'satorii' ) .'</label></div>
+									<div class="form-input"><input id="url" name="url" class="text" type="text" value="'. esc_url($comment_author_url) .'" size="30" maxlength="50" tabindex="5" /></div>
+							</div>'
+							),
+							'comment_field' => '<div class="yui-u" id="form-textarea"><div class="form-label"><label for="comment">'. __( 'Comment', 'satorii' ) .'</label></div>
+								<div class="form-textarea"><textarea id="comment" name="comment" class="text required" cols="45" rows="8" tabindex="6"></textarea></div></div></div>',
+							'id_form' => 'commentform',
+							'id_submit' => 'submit'
+						)); ?>
 					</div><!-- .formcontainer -->
 <?php endif // REFERENCE: if ( get_option('comment_registration') && !$user_ID ) ?>
 
