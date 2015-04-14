4a18daedb352937bf41cc89426e7e6326f789707
Felipe Lavín Z <felipe@yukei.net>
2015-04-14 00:35:21 -0300

    responsive navigation

--- header.php
+++ header.php
@@ -1,22 +1,40 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes() ?>>
-<head profile="http://gmpg.org/xfn/11">
-	<title><?php wp_title( '-', true, 'right' ); echo esc_html( get_bloginfo('name'), 1 ) ?></title>
-	<meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
-	<!--[if (lt IE 7) | (IE 8)]>
-		<style type="text/css">#wrapper{ display: table; height: 100% }</style>
-	<![endif]-->
-	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); /* Threaded comments */?>
-	<?php wp_head() /* For plugins */?>
+<?php
+/**
+ * The header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package satoriii
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<!--[if (lt IE 7) | (IE 8)]>
+	<style type="text/css">#wrapper{ display: table; height: 100% }</style>
+<![endif]-->
+<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); /* Threaded comments */?>
+<?php wp_head(); ?>
 </head>
 <body <?php body_class() ?>>
 <div id="wrapper" class="hfeed">
 	<div id="inner-wrap">
-		<div id="header">
-			<h1 id="blog-title"><span><a href="<?php echo home_url() ?>/" title="<?php echo esc_attr( get_bloginfo('name') ) ?>" rel="home"><?php bloginfo('name') ?></a></span></h1>
-			<div id="blog-description"><?php bloginfo('description') ?></div>
-		</div><!--  #header -->
+		<div id="page-header">
+			<div class="container-fluid">
+				<h1 id="blog-title"><span><a href="<?php echo home_url() ?>/" title="<?php echo esc_attr( get_bloginfo('name') ) ?>" rel="home"><?php bloginfo('name') ?></a></span></h1>
+				<div id="blog-description" class="visible-md visible-lg"><?php bloginfo('description') ?></div>
+			</div>
+		</div><!-- #page-header -->
 		<div id="access">
-			<div class="skip-link"><a href="#content" title="<?php _e( 'Skip to content', 'satorii' ) ?>"><?php _e( 'Skip to content', 'satorii' ) ?></a></div>
+			<div class="sr-only sr-only-focusable skip-link"><a href="#content" title="<?php _e( 'Skip to content', 'satorii' ) ?>"><?php _e( 'Skip to content', 'satorii' ) ?></a></div>
+			<button type="button" id="globalnav-toggle" class="navbar-toggle visible-xs" data-target="#globalnav">
+				<span class="sr-only">Toggle Navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
 			<?php satorii_globalnav(); ?>
 		</div><!-- #access -->
--- js/satorii.js
+++ js/satorii.js
@@ -1,17 +1,26 @@
 jQuery(document).ready(function($) {
-	$('div#content div[id^=post]').each(function() {
-		var postId = $(this).attr('id');
-		$('div#'+postId+' a[href$=".png"]').addClass('fancybox').attr('rel', postId);
-		$('div#'+postId+' a[href$=".gif"]').addClass('fancybox').attr('rel', postId);
-		$('div#'+postId+' a[href$=".jpg"]').addClass('fancybox').attr('rel', postId);
-		$('div#'+postId+' a[href$=".PNG"]').addClass('fancybox').attr('rel', postId);
-		$('div#'+postId+' a[href$=".GIF"]').addClass('fancybox').attr('rel', postId);
-		$('div#'+postId+' a[href$=".JPG"]').addClass('fancybox').attr('rel', postId);
+	$('#globalnav-toggle').on('click', function(event){
+		$('#globalnav, #globalnav-toggle').toggleClass('active');
+		$('#page-header, #container, body').toggleClass('nav-showing');
+		event.preventDefault();
 	});
-	$('a.fancybox').fancybox({
-		'overlayShow' : true,
-		'hideOnContentClick': true
+	$(document).on('click', '#globalnav.active', function( event ){
+		$('#globalnav-toggle').trigger('click');
+		event.preventDefault();
 	});
-	$('table tr').removeClass('odd even');
-	$('table tr:even').addClass('odd');
+	// $('div#content div[id^=post]').each(function() {
+	// 	var postId = $(this).attr('id');
+	// 	$('div#'+postId+' a[href$=".png"]').addClass('fancybox').attr('rel', postId);
+	// 	$('div#'+postId+' a[href$=".gif"]').addClass('fancybox').attr('rel', postId);
+	// 	$('div#'+postId+' a[href$=".jpg"]').addClass('fancybox').attr('rel', postId);
+	// 	$('div#'+postId+' a[href$=".PNG"]').addClass('fancybox').attr('rel', postId);
+	// 	$('div#'+postId+' a[href$=".GIF"]').addClass('fancybox').attr('rel', postId);
+	// 	$('div#'+postId+' a[href$=".JPG"]').addClass('fancybox').attr('rel', postId);
+	// });
+	// $('a.fancybox').fancybox({
+	// 	'overlayShow' : true,
+	// 	'hideOnContentClick': true
+	// });
+	// $('table tr').removeClass('odd even');
+	// $('table tr:even').addClass('odd');
 });
